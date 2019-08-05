aed90c90f3181b17d878dbbb13b1a6611c796756
Jelani Woods <jelani@firstdraft.com>
2019-08-05 21:25:55 +0000

    Venue specs

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -13,4 +13,26 @@
 
 
 class Venue < ApplicationRecord
+  
+  def city
+    City.where(id: self.city_id).first
+  end
+  
+  def performances
+    Performance.where(venue_id: self.id)
+  end
+  
+  def artists
+    artist_ids = self.performances.pluck(:artist_id)
+    Artist.where(id: artist_ids)
+  end
+  
+  def Venue.smallest
+    Venue.where.not(size: nil).order(:size).first
+  end
+  
+  def Venue.largest
+    Venue.all.order(:size).last
+  end
+  
 end
